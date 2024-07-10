41a6cee4dec3cd8eeb3646aaa09a4c8e85c51d4e
rhyde23 <reginaldjhyde@gmail.com>
2024-07-10 16:26:35 -0500

    Changing execute_regression_model.py so that it groups similar positions together for analysis.

--- execute_regression_model.py
+++ execute_regression_model.py
@@ -1,35 +1,71 @@
+#Executing the Regression Model to identify overvalued/undervalued players
+
+#Import pandas library to load dataframes
 import pandas as pd
 
+#Import sklearn library to perform ML models
 import sklearn
 
+#Import the LinearRegression class from sklearn to perform a linear regression
 from sklearn.linear_model import LinearRegression
 
+#Import sqlite3 module to establish connection to PlayerStats.db
 import sqlite3
 
+#Import the SimpleImputer class from sklearn to replace all null values with 0
 from sklearn.impute import SimpleImputer
 
+groups_of_similar_positions = [
+    ['Centre-Forward'],
+    ['Right Winger', 'Left Midfield', 'Right Midfield', 'Left Winger'],
+    ['Central Midfield'],
+    ['Defensive Midfield'],
+    ['Attacking Midfield', 'Second Striker'],
+    ['Left-Back', 'Right-Back'],
+    ['Centre-Back']
+]
 
-database_connection = sqlite3.connect('PlayerStats.db')
+def build_query(group_of_similar_positions) :
+    
+    return "SELECT * FROM "+table_name+" WHERE POSITION == 'Centre-Back' OR POSITION == 'Right-Back'"
 
-query = "SELECT * FROM Premier_League"
 
-player_data = pd.read_sql(query, database_connection)
+def perform_regressions_on_league(table_name) :
+    
+    #Connect to the Player Stats Database
+    database_connection = sqlite3.connect('PlayerStats.db')
 
-filtered_by_position = player_data.query("POSITION == 'Centre-Back'")
+    #Query to select all data from a given table
+    query = "SELECT * FROM "+table_name#+" WHERE POSITION == 'Centre-Back' OR POSITION == 'Right-Back'"
 
-X = filtered_by_position[list(filtered_by_position)[3:]]
-y = filtered_by_position[['MARKET_VALUE']]
+    #Read the SQL table and store into "player_data" using pandas' function read_sql
+    player_data = pd.read_sql(query, database_connection)
 
-imputer = SimpleImputer(strategy='constant', fill_value=0)
-X = imputer.fit_transform(X)
+    #Filter by
 
-linear_regression = LinearRegression(fit_intercept = True)
-linear_regression.fit(X, y)
+    print(player_data["POSITION"].unique())
+    #quit()
+    
+    #filtered_by_position = player_data.query("POSITION == 'Centre-Back' OR POSITION == 'Right-Back' ")
+    """
+    print(player_data)
+    quit()
+    
+    X = filtered_by_position[list(filtered_by_position)[3:]]
+    y = filtered_by_position[['MARKET_VALUE']]
 
-prediction = linear.predict(X)
+    imputer = SimpleImputer(strategy='constant', fill_value=0)
+    X = imputer.fit_transform(X)
 
-filtered_by_position["PREDICTED_PRICE"] = prediction
-filtered_by_position["DIFFERENCE"] = filtered_by_position["PREDICTED_PRICE"]-filtered_by_position["MARKET_VALUE"]
+    linear_regression = LinearRegression(fit_intercept = True)
+    linear_regression.fit(X, y)
 
-sorted_by_difference = filtered_by_position.sort_values(["DIFFERENCE"])
-print(sorted_by_difference[['NAME', 'DIFFERENCE']].tail(5))
+    prediction = linear.predict(X)
+
+    filtered_by_position["PREDICTED_PRICE"] = prediction
+    filtered_by_position["DIFFERENCE"] = filtered_by_position["PREDICTED_PRICE"]-filtered_by_position["MARKET_VALUE"]
+
+    sorted_by_difference = filtered_by_position.sort_values(["DIFFERENCE"])
+    print(sorted_by_difference[['NAME', 'DIFFERENCE']].tail(5))
+    """
+
